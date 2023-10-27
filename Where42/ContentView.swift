a973882d7a0ad3c8512332dfb1372bab99412e1a
Dongho Hyun <ehdghho@kakao.com>
2023-10-27 14:27:58 +0900

    Initial Commit

---
+++ Where42/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Where42
+//
+//  Created by 현동호 on 10/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Where42/Where42App.swift
@@ -0,0 +1,17 @@
+//
+//  Where42App.swift
+//  Where42
+//
+//  Created by 현동호 on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct Where42App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
