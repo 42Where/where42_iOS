6b74ed4166e713a8949b7659f270e1ad28555fd8
Dongho Hyun <ehdghho@kakao.com>
2023-12-27 17:04:54 +0900

    [WIP] Delete group user

---
+++ Where42/View/Home/GroupEditView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupEditView.swift
+//  Where42
+//
+//  Created by 현동호 on 12/23/23.
+//
+
+import SwiftUI
+
+struct GroupEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupEditView()
+}
