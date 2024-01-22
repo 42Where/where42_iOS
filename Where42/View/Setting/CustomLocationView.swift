080c27890169ee82e5b6b23986cd8d3049bca592
Dongho Hyun <ehdghho@kakao.com>
2024-01-22 22:21:15 +0900

    [WIP]: CustomLocation

---
+++ Where42/View/Setting/CustomLocationView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomLocationView.swift
+//  Where42
+//
+//  Created by 현동호 on 1/20/24.
+//
+
+import SwiftUI
+
+struct CustomLocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomLocationView()
+}
