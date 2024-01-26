2af25235051c625caf8c0bf5638b70877ad8bef4
Dongho Hyun <ehdghho@kakao.com>
2024-01-26 18:03:35 +0900

    [FEAT] update Search View
    #9

---
+++ Where42/View/Search/SearchMemberInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchMemberInfoView.swift
+//  Where42
+//
+//  Created by 현동호 on 1/26/24.
+//
+
+import SwiftUI
+
+struct SearchMemberInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchMemberInfoView()
+}
