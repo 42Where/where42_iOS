4dbbb7cf97014c45a2dc2d0867bcbc7ac62a3894
ch <ckdgus1120@gmail.com>
2024-11-17 18:11:35 +0900

    [DESIGN] Announcement icon assets added

---
+++ Where42/View/Announcement/AnnouncementView.swift
@@ -0,0 +1,18 @@
+//
+//  AnnouncementView.swift
+//  Where42
+//
+//  Created by 이창현 on 11/17/24.
+//
+
+import SwiftUI
+
+struct AnnouncementView: View {
+    var body: some View {
+        Text("Announcement!!!")
+    }
+}
+
+#Preview {
+    AnnouncementView()
+}
