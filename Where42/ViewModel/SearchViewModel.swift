e113a77608103fe1045da5dd4dae8bafabfdcd14
ch <ckdgus1120@gmail.com>
2025-06-20 20:42:45 +0900

    [HOTFIX] solving capture of non-sendable type in @Sendable Closure - ViewModels

--- Where42/ViewModel/SearchViewModel.swift
+++ Where42/ViewModel/SearchViewModel.swift
@@ -40,36 +40,40 @@ class SearchViewModel: ObservableObject {
             searchStatus = .apiCalled
             Task {
                 await searchMemeber(keyWord: keyWord)
-                DispatchQueue.main.async { [weak self] in
-                    guard let self = self else { return }
-                    self.searchStatus = .waiting
-                    if self.name == "" {
-                        self.searching = []
-                    }
-                }
+                await setSearchUI()
             }
+        }
+    }
+    
+    @MainActor
+    private func setSearchUI() {
+        self.searchStatus = .waiting
+        if self.name == "" {
+            self.searching = []
         }
     }
 
     func searchMemeber(keyWord: String) async {
         do {
             if let searchingMember = try await memberAPI.search(keyWord: keyWord) {
-                DispatchQueue.main.async { [weak self] in
-                    guard let self = self else { return }
-                    if self.searchStatus == .apiCalled {
-                        self.searching = searchingMember.map { memberInfo in
-                            var member = memberInfo
-                            if self.selectedMembers.contains(where: { $0.intraId == memberInfo.intraId }) {
-                                member.isCheck = true
-                            }
-                            return member
-                        }
-                    }
-                }
+                await setSearchedMembersUI(searchingMember)
             }
         } catch {
             DispatchQueue.main.async {
                 MainViewModel.shared.toast = Toast(title: "잠시 후 다시 시도해 주세요")
+            }
+        }
+    }
+    
+    @MainActor
+    private func setSearchedMembersUI(_ searchingMember: [MemberInfo]) {
+        if self.searchStatus == .apiCalled {
+            self.searching = searchingMember.map { memberInfo in
+                var member = memberInfo
+                if self.selectedMembers.contains(where: { $0.intraId == memberInfo.intraId }) {
+                    member.isCheck = true
+                }
+                return member
             }
         }
     }
@@ -83,10 +87,7 @@ class SearchViewModel: ObservableObject {
                     MainViewModel.shared.toast = Toast(title: "잠시 후 다시 시도해 주세요")
                 }
             } else {
-                DispatchQueue.main.async { [weak self] in
-                    guard let self = self else { return }
-                    self.initSearchingAfterAdd()
-                }
+                await initSearchingAfterAdd()
             }
         } catch NetworkError.Reissue {
             DispatchQueue.main.async {
@@ -99,6 +100,7 @@ class SearchViewModel: ObservableObject {
         return true
     }
 
+    @MainActor
     func initSearchingAfterAdd() {
         withAnimation {
             searching = searching.map { member in
